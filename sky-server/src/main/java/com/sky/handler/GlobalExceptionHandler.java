16ac0e09f9ede2660501c267ab3e4c279e184b9a
Webster <weixiao.chen.98@foxmail.com>
2023-09-02 22:57:05 +0800

    enhance the logic to handle the exception for adding an existed employee

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import static com.sky.constant.MessageConstant.ALREADY_EXISTED;
+import static com.sky.constant.MessageConstant.UNKNOWN_ERROR;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -12,6 +18,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException exception) {
+        // error message template: Duplicate entry 'zhangsan' for key 'employee.idx_username'
+        String message = UNKNOWN_ERROR;
+        if (exception.getMessage().contains("Duplicate entry")) {
+            String[] tokens = exception.getMessage().split(" ");
+            message = tokens[2] + ALREADY_EXISTED;
+        }
+        return Result.error(message);
+    }
 
     /**
      * 捕获业务异常
